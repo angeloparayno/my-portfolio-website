8de9b0775d4e04a4127a1be1c591a47514cd66b7
Angelo Parayno <angeloparayno.dev@gmail.com>
2023-08-15 22:23:12 -0700

    major update

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+  const element = document.getElementById('animationElement');
+
+  // Apply random pixel shifts as CSS custom properties
+  element.style.setProperty('--shiftX', `${Math.random() * 40 - 20}px`);
+  element.style.setProperty('--shiftY', `${Math.random() * 40 - 20}px`);
+
